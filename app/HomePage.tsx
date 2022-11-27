e373b3ef280f45fbd8056e17f1ee6e33ec220ce1
caturbgs <caturbagas4172@gmail.com>
2022-11-27 21:52:12 +0700

    feat: add create and update data function

--- app/HomePage.tsx
+++ app/HomePage.tsx
@@ -2,40 +2,73 @@
 
 import { Post } from '@types';
 import { useRouter } from 'next/navigation';
-// import { useCallback, useEffect, useState } from 'react';
 
 export default function HomePage({ data }: { data: Post[] }) {
   const router = useRouter();
-  // const [filteredData, setFilteredData] = useState<Post[]>([]);
 
-  // const filterData = useCallback(() => {
-  //   if (data) {
-  //     setFilteredData(data.slice(0, 10));
-  //   }
-  // }, [data]);
+  const handleCreate = async () => {
+    const lastId = data[ data.length - 1 ].id + 1;
+    await fetch(`http://localhost:8000/posts/`, {
+      method: 'POST',
+      body: JSON.stringify({
+        id: lastId,
+        title: `New Title ${lastId}`,
+        body: `New Body ${lastId}`,
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    });
 
-  // useEffect(() => {
-  //   filterData();
-  // }, [filterData]);
+    router.refresh();
+  };
 
-  const filteredData = (data || []).slice(0, 10);
+  const handleUpdate = async (id: number) => {
+    await fetch(`http://localhost:8000/posts/${id}`, {
+      method: 'PUT',
+      body: JSON.stringify({
+        id,
+        title: `Update Title ${id}`,
+        body: `Update Body ${id}`,
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    });
+
+    router.refresh();
+  };
 
   return (
     <div>
       <div className="flex justify-between mb-5">
         <h1>Isi Data dari API</h1>
-        <button
-          className="bg-emerald-300 hover:bg-white text-white hover:text-emerald-300 border-2 border-emerald-300 rounded-lg py-2 px-4"
-          onClick={() => router.refresh()}>
-          Revalidate Data
-        </button>
+        <div>
+          <button
+            className="bg-emerald-300 hover:bg-white text-white hover:text-emerald-300 border-2 border-emerald-300 rounded-lg py-2 px-4 mx-1"
+            onClick={() => router.refresh()}>
+            Revalidate Data
+          </button>
+          <button
+            className="bg-emerald-300 hover:bg-white text-white hover:text-emerald-300 border-2 border-emerald-300 rounded-lg py-2 px-4 mx-1"
+            onClick={() => handleCreate()}>
+            Create Data
+          </button>
+        </div>
       </div>
-      <ul>
-        {/* {filteredData.map((d) => ( */}
-        {filteredData.map((d) => (
-          <li key={d.id}>{d.title}</li>
+      <div className="grid">
+        {data.map((d) => (
+          <div key={d.id} className="flex justify-between items-center mb-1 px-2">
+            <div className='flex'>
+              <p className="bg-orange-200 text-xl px-2 font-semibold rounded-md mr-2">{d.title}</p>
+              <p className="bg-orange-300 text-md px-2 rounded-md">{d.body}</p>
+            </div>
+            <button onClick={() => handleUpdate(d.id)} className="bg-indigo-600 text-white rounded-md px-2 py-1">
+              Update
+            </button>
+          </div>
         ))}
-      </ul>
+      </div>
     </div>
   );
 }
