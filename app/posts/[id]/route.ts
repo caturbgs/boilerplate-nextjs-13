8d8b98c81f22b9496c9ce76af8274933a01984bb
caturbgs <caturbagas4172@gmail.com>
2023-04-13 00:17:10 +0700

    fix: route handler api

--- app/posts/[id]/route.ts
+++ app/posts/[id]/route.ts
@@ -1,20 +1,20 @@
-import { PrismaClient } from '@prisma/client';
-import { NextRequest, NextResponse } from 'next/server';
+import { Prisma, PrismaClient } from '@prisma/client';
+import { NextResponse } from 'next/server';
 
 const prisma = new PrismaClient();
 
 // create route handler for PUT /api/posts/:id using prisma
-export default async function PUT(req: NextRequest) {
-  const { body } = req;
-  const id = req.nextUrl.searchParams.get('id');
+export async function PUT(req: Request, {params}: {params: {id: string}}) {
+  const res: Prisma.postUpdateInput = await req.json();
+  const id = params.id;
 
   try {
     await prisma.post.update({
-      where: { id: Number(id) },
-      data: body as any,
+      where: { id },
+      data: { title: res.title, body: res.body },
     });
 
-    return NextResponse.json({ message: "Success update post!" });
+    return NextResponse.json({ message: 'Success update post!' });
   } catch (error) {
     console.error(error);
     return NextResponse.json({ message: 'Something went wrong!' });
--- app/posts/route.ts
+++ app/posts/route.ts
@@ -1,20 +1,29 @@
-import { PrismaClient } from '@prisma/client';
-import { NextRequest, NextResponse } from 'next/server';
+import { Prisma, PrismaClient } from '@prisma/client';
+import { NextResponse } from 'next/server';
 
 const prisma = new PrismaClient();
 
-export default async function GET(req: NextRequest) {
-  const { body } = req;
+export async function POST(req: Request) {
+  const res: Prisma.postCreateInput = await req.json();
 
   try {
     await prisma.post.create({
-      data: body as any,
+      data: {
+        title: res.title,
+        body: res.body,
+        user: {
+          create: {
+            name: 'John Doe',
+            email: 'johndoe@gmail.com',
+          },
+        }
+      },
     });
 
-    return NextResponse.json({ message: "Success create post!" });
+    return NextResponse.json({ message: 'Success create post!' });
     // return new NextResponse(JSON.stringify({ message: "Success create post!" }), {status: 200, });
   } catch (error) {
     console.error(error);
-    return NextResponse.json({ message: 'Something went wrong!' });
+    return NextResponse.json({ message: 'Something went wrong!' }, {status: 500});
   }
 }
