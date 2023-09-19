79ee3ff7ce5f2583e145417d4080bce3acbe7912
caturbgs <caturbagas11@gmail.com>
2023-09-19 16:18:00 +0700

    feat: upgrade next version config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,30 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   reactStrictMode: true,
   swcMinify: true,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   experimental: {
-    appDir: true,
-    typedRoutes: true,
+    serverActions: true,
+  },
+  images: {
+    domains: [
+      'localhost', // for local development
+    ],
+  },
+  // improve performance on local development
+  // referemce: https://nextjs.org/docs/architecture/nextjs-compiler#modularize-imports
+  modularizeImports: {
+    // 'lucide-react': {
+    //   transform: 'lucide-react/dist/esm/icons/{{ kebabCase member}}.js',
+    // },
+    // '@radix-ui': {
+    //   transform: '@radix-ui/react-{{ member }}',
+    // },
   },
 };
 
